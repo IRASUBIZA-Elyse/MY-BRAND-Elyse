d8f990515643ced8f4d93ceb2f5f126557d8710b
IRASUBIZA-Elyse <h.irasubizaelyse@gmail.com>
2024-01-18 13:13:39 +0200

    added the js file  while the opacity on navbar in it

---
+++ main.js
@@ -0,0 +1,9 @@
+// adding sticky transparent menu
+
+window.addEventListener("scroll", function () {
+  if (window.scrollY > 150) {
+    document.querySelector("#navbar").style.opacity = 0.9;
+  } else {
+    document.querySelector("#navbar").style.opacity = 1;
+  }
+});
