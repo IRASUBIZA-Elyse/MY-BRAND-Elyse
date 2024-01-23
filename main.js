10cc387c53c630e21fe4a3ff5e24c3f4d94e6178
IRASUBIZA-Elyse <h.irasubizaelyse@gmail.com>
2024-01-23 10:25:32 +0200

    added the singlepage blog and all blogs page

--- main.js
+++ main.js
@@ -2,7 +2,7 @@
 
 window.addEventListener("scroll", function () {
   if (window.scrollY > 150) {
-    document.querySelector("#navbar").style.opacity = 0.9;
+    document.querySelector("#navbar").style.opacity = 0.8;
   } else {
     document.querySelector("#navbar").style.opacity = 1;
   }
