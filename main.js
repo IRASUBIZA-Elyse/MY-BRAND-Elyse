b4250381961d84adc91fe02237fd82355a3b93a3
IRASUBIZA-Elyse <h.irasubizaelyse@gmail.com>
2024-01-24 11:56:29 +0200

    added the dashboard and all responsiveness of single page

--- main.js
+++ main.js
@@ -3,7 +3,9 @@
 window.addEventListener("scroll", function () {
   if (window.scrollY > 150) {
     document.querySelector("#navbar").style.opacity = 0.8;
+    document.querySelector(".footer2").style.opacity = 0.8;
   } else {
     document.querySelector("#navbar").style.opacity = 1;
+    document.querySelector(".footer2").style.opacity = 0.8;
   }
 });
