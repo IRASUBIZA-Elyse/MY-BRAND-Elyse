e0ff06e9c04e74a341fbf2ce666ad9719c0eb727
IRASUBIZA-Elyse <h.irasubizaelyse@gmail.com>
2024-03-05 13:43:17 +0200

    removed author on updating of blog

--- js/updateBlog.js
+++ js/updateBlog.js
@@ -6,7 +6,7 @@ const allBlogs = JSON.parse(localStorage.getItem("blogs")) || [];
 const choosenBlogIndex = allBlogs.findIndex((blog) => blog.id == blogId);
 // console.log(choosenBlog.id);
 const title = document.querySelector("#titleUpdate"); // title inpit
-const author = document.querySelector("#updateAuthor"); // author
+//const author = document.querySelector("#updateAuthor"); // author
 const date = document.querySelector("#upDate"); // date
 const content = document.querySelector("#summernote"); //text editor
 let formUpdateBlog = document.querySelector("#updateBlogForm"); // update form id
@@ -15,7 +15,7 @@ if (choosenBlogIndex !== -1) {
   const choosenBlog = allBlogs[choosenBlogIndex];
   console.log(choosenBlog);
   title.value = choosenBlog.title;
-  author.value = choosenBlog.author;
+  //author.value = choosenBlog.author;
   date.value = choosenBlog.date;
   content.value = choosenBlog.content;
   console.log("ok");
@@ -27,7 +27,7 @@ if (choosenBlogIndex !== -1) {
     const updatedBlog = {
       ...choosenBlog,
       title: title.value,
-      author: author.value,
+      //author: author.value,
       date: date.value,
       content: content.value,
     };
@@ -35,7 +35,7 @@ if (choosenBlogIndex !== -1) {
     allBlogs[choosenBlogIndex] = updatedBlog;
     localStorage.setItem("blogs", JSON.stringify(allBlogs));
     title.value = "";
-    author.value = "";
+    //author.value = "";
     date.value = "";
     content.value = "";
     windows.location.href = "./adminBlogs.html";
